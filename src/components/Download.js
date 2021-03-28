247aef669f5376e9131b0358fb54304aabd8dc35
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-03-28 04:19:08 -0300

    Added temporal cv link

--- src/components/Download.js
+++ src/components/Download.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Download = () => {
     return (
         <div className="download">
-            <a href='https://www.google.com/' target='_blank' rel="noopener noreferrer">
+            <a href='https://docs.google.com/document/d/1WJSO0btgun2GkG2YSvqSoGJ9ru65TV9kmhMEfbDNJ-A/edit?usp=sharing' target='_blank' rel="noopener noreferrer">
                 <i className={'fa fa-download'}></i>
                 <p>Descargar CV</p>
             </a>
