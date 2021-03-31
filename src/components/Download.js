a32c816152dc79d98d047962a392c5d6bef0b1e5
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-03-30 21:45:35 -0300

    Added pdf cv

--- src/components/Download.js
+++ src/components/Download.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Download() {
     return (
         <div className="download">
-            <a href='https://docs.google.com/document/d/1WJSO0btgun2GkG2YSvqSoGJ9ru65TV9kmhMEfbDNJ-A/edit?usp=sharing' target='_blank' rel="noopener noreferrer">
+            <a href='./CV_Urtubey_Carlos.pdf' target='_blank' rel="noopener noreferrer">
                 <i className={'fa fa-download'}></i>
                 <p>Descargar CV</p>
             </a>
