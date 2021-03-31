e56c4d097d9fa72c6d16cfd18edf8b0bbd0c99d6
Carlos Urtubey <urtubeycarlos.0510@gmail.com>
2021-03-30 22:54:09 -0300

    Updated CV

--- src/components/Download.js
+++ src/components/Download.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Download() {
     return (
         <div className="download">
-            <a href='https://drive.google.com/file/d/1P-t5fFsjeo8mWg_AWOvb_KPOMaC-nPxX/view?usp=sharing' target='_blank' rel="noopener noreferrer">
+            <a href='https://drive.google.com/file/d/1m1w6VTpOuhLo6VbKdpmMmLc_gGmKGrnx/view?usp=sharing' target='_blank' rel="noopener noreferrer">
                 <i className={'fa fa-download'}></i>
                 <p>Descargar CV</p>
             </a>
